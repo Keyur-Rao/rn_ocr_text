f71a9404ea8fce7a64ea017fe6c1d0db891ed72b
Keyur-Rao <keyur@raoinformationtechnology.com>
2023-12-17 10:20:08 +0530

    Material bottom navigation set-up complete with react-native-paper instead of materialBottomTabNavigation

---
+++ app/navigator/MyTabs.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from '../screens/Home';
+import Add from '../screens/Add';
+import Settings from '../screens/Settings';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: '#694fad' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Add"
+        component={Add}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
+
+export default MyTabs;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  //To get smaller bundle size by excluding modules you don't use, The plugin automatically rewrites the import statements so that only the modules you use are imported instead of the whole library.
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
